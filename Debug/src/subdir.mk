5c6ba90ba4c3b614530ffc863b2a4a77846ba140
dnoack <dnoack@work>
2015-01-27 14:33:14 +0100

    Adding eclipse autogenerated make files nad Eclipse Config File for Project.

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Aardvark.cpp \
+../src/JsonRPC.cpp \
+../src/RPCServer.cpp \
+../src/test.cpp 
+
+OBJS += \
+./src/Aardvark.o \
+./src/JsonRPC.o \
+./src/RPCServer.o \
+./src/test.o 
+
+CPP_DEPS += \
+./src/Aardvark.d \
+./src/JsonRPC.d \
+./src/RPCServer.d \
+./src/test.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/home/dnoack/libs/rapidjson/include/rapidjson -I/home/dnoack/libs/zeromq-4.0.5/include -I"/home/dnoack/workspace/Plugin-Server/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
