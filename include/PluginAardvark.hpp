db6f8b3120ede15a1335d96a43639bbdcf379eeb
dnoack <dnoack@work>
2015-02-06 14:10:47 +0100

    Multi Client Uds Server for one Request per Client at time.
    Former-commit-id: 1124c1d8c508d533f5b478df2afaea610554dc4e

--- include/PluginAardvark.hpp
+++ include/PluginAardvark.hpp
@@ -24,7 +24,6 @@ class PluginAardvark{
 		PluginAardvark();
 		~PluginAardvark();
 
-		bool registerToRSD();
 		void startListening();
 
 	private:
---
+++ src/PluginAardvark.cpp
@@ -0,0 +1,62 @@
+/*
+ * PluginAardvark.cpp
+ *
+ *  Created on: 05.02.2015
+ *      Author: dnoack
+ */
+
+
+#include "PluginAardvark.hpp"
+
+PluginAardvark::PluginAardvark()
+{
+	detectDevices();
+	this->uds_reg = new UdsServer(SERVER_MODE, UDS_REGISTER_TO_RSD_PATH, sizeof(UDS_REGISTER_TO_RSD_PATH));
+	uds_reg->startCom();
+}
+
+
+
+PluginAardvark::~PluginAardvark()
+{
+	for(unsigned int i = 0; i < deviceList.size(); i++)
+		delete deviceList[i];
+}
+
+
+
+
+void PluginAardvark::startListening()
+{
+	uds_com = new UdsServer(SERVER_MODE, UDS_COM_PATH, sizeof(UDS_COM_PATH));
+}
+
+
+
+void PluginAardvark::detectDevices()
+{
+	u16 devices[EXPECTED_NUM_OF_DEVICES];
+	u32 unique_ids[EXPECTED_NUM_OF_DEVICES];
+
+	aa_find_devices_ext(EXPECTED_NUM_OF_DEVICES, devices, EXPECTED_NUM_OF_DEVICES, unique_ids);
+
+	for(int i = 0; i < EXPECTED_NUM_OF_DEVICES; i++)
+	{
+		if(unique_ids[i] != 0)
+		{
+			//devices will be deleted within destructor
+			deviceList.push_back(new RemoteAardvark());
+		}
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	PluginAardvark* test = new PluginAardvark();
+	//test->registerToRSD();
+	while(1)
+		sleep(3);
+	delete test;
+}
+
