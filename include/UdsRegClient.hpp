ea725b56048ebdf36b0b2d88b37d7e609be2def2
dnoack <dnoack@work>
2015-04-30 08:46:36 +0200

    Rename eclipse project.
    Former-commit-id: 140a7006c5f09f33680be479bf1fed082b2f1ee1

--- include/UdsRegClient.hpp
+++ include/UdsRegClient.hpp
@@ -1,5 +1,5 @@
-#ifndef INCLUDE_UDSREGCLIENT_HPP_
-#define INCLUDE_UDSREGCLIENT_HPP_
+#ifndef AARDVARK_PLUGIN_INCLUDE_UDSREGCLIENT_HPP_
+#define AARDVARK_PLUGIN_INCLUDE_UDSREGCLIENT_HPP_
 
 #include <sys/un.h>
 #include <sys/socket.h>
@@ -47,4 +47,4 @@ class UdsRegClient{
 		const char* pluginPath;
 };
 
-#endif /* INCLUDE_UDSREGCLIENT_HPP_ */
+#endif /* AARDVARK_PLUGIN_INCLUDE_UDSREGCLIENT_HPP_ */
