86baeed2533514b5301b66092937c62823abe295
Dave <Dave@linux-1sjv.fritz.box>
2015-05-08 12:31:39 +0200

    Removed variables that are now in WorkerInterface, removed unused functions. Made sure every destructor calls deleteReceiveQueue().
    Former-commit-id: c5735f8b87809e37a3cb43d5c7f7005eec6b204a

---
+++ include/UdsRegWorker.hpp
@@ -0,0 +1,53 @@
+
+#ifndef INCLUDE_UDSREGWORKER_HPP_
+#define INCLUDE_UDSREGWORKER_HPP_
+
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstring>
+
+
+#include "JsonRPC.hpp"
+#include "WorkerInterface.hpp"
+#include "WorkerThreads.hpp"
+
+
+
+class UdsRegWorker : public WorkerInterface<string>, public WorkerThreads{
+
+	public:
+		UdsRegWorker(int socket);
+		~UdsRegWorker();
+
+
+		void sendAnnounceMsg(const char* pluginName, int pluginNumber, const char* pluginPath);
+
+		int transmit(const char* data, int size);
+		int transmit(string* msg);
+
+	private:
+
+		virtual void thread_listen();
+		virtual void thread_work();
+
+		JsonRPC* json;
+		const char* error;
+		Value* currentMsgId;
+
+		enum REG_STATE{NOT_ACTIVE, ANNOUNCED, REGISTERED, ACTIVE, BROKEN};
+		unsigned int state;
+
+
+		void processRegistration();
+		bool handleAnnounceACKMsg(string* msg);
+		const char* createRegisterMsg();
+		bool handleRegisterACKMsg(string* msg);
+		const char* createPluginActiveMsg();
+
+};
+
+#endif /* INCLUDE_UDSREGWORKER_HPP_ */
